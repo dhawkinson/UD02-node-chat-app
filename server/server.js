d9b73139d9f985af8bd5140ec77fe533b8574a4e
Doug Hawkinson <doug5solas@gmail.com>
2018-03-31 15:56:19 -0700

    Enhanced UX for Send Location button

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('Ack from server');
+        callback();
     });
 
     //  listen for event coming from client - geolocation
